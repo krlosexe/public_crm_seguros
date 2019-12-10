f9ccd8ba7003706a3c75e1ab6c8c008655a7b2a1
krlosexe <cardenascarlos18@gmail.com>
2019-12-10 17:27:35 -0500

    AVACNES 10-12-2019

--- js/funciones.js
+++ js/funciones.js
@@ -876,3 +876,37 @@ function GetRamos(select){
     }
   });
 }
+
+
+
+function number_format(amount, decimals) {   
+  amount += ''; // por si pasan un numero en vez de un string
+  amount = parseFloat(amount.replace(/[^0-9\.]/g, ''));
+  // elimino cualquier cosa que no sea numero o punto 
+  decimals = decimals || 0; // por si la variable no fue fue pasada  
+  // si no es un numero o es igual a cero retorno el mismo cero 
+  if (isNaN(amount) || amount === 0)      
+      return parseFloat(0).toFixed(decimals);     
+      // si es mayor o menor que cero retorno el valor formateado como numero   
+      amount = '' + amount.toFixed(decimals);   
+      var amount_parts = amount.split('.'),    
+      regexp = /(\d+)(\d{3})/;       
+      while (regexp.test(amount_parts[0]))  
+      amount_parts[0] = amount_parts[0].replace(regexp, '$1' + ',' + '$2'); 
+      return amount_parts.join('.');  
+}
+
+$(".monto_formato_decimales").change(function() {   
+  if($(this).val() != ""){  
+    $(this).val(number_format($(this).val(), 2));   
+  }       
+});
+
+
+
+function inNum(monto) {
+	var cantidad           = monto;
+	var myNumeral          = numeral(cantidad);
+    return myNumeral.value();
+}
+
